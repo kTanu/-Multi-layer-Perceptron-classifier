1a3877bd4482c3eee3d0597936a20877536fbf5d
kTanu <92226337+kTanu@users.noreply.github.com>
2022-10-19 15:40:26 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+pima=pd.read_csv(r"C:\Users\Node-1\Desktop\machinelearning\data.csv")
+X=np.array(pima.iloc[:,:-1])
+y=np.array(pima.iloc[:,-1])
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=1)
+clf=MLPClassifier()
+clf=clf.fit(X_train,y_train)
+y_pred=clf.predict(X_test)
+print("accuracy",metrics.accuracy_score(y_test,y_pred))
+    
